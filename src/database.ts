a9d7944ddc12a441c88b9f90f30c4b259706deb2
thiagostudier <thiago.studier9@gmail.com>
2024-03-03 16:37:16 -0300

    main: update database

--- src/database.ts
+++ src/database.ts
@@ -1,11 +1,14 @@
+import { Knex, knex as setupKnex } from 'knex'
 import { env } from './env'
-import { knex as setupKnex, Knex } from 'knex'
 
 export const config: Knex.Config = {
   client: env.DATABASE_CLIENT,
-  connection: env.DATABASE_CLIENT === 'sqlite' ? {
-    filename: env.DATABASE_URL
-  } : env.DATABASE_URL,
+  connection:
+    env.DATABASE_CLIENT === 'sqlite'
+      ? {
+          filename: env.DATABASE_URL
+        }
+      : env.DATABASE_URL,
   useNullAsDefault: true,
   migrations: {
     extension: 'ts',
